ebea2e7792e5cfbba6122d1ea1721e05e47a9a1a
chohanreddyp@gmail.com <chohanreddyp@gmail.com>
2025-12-11 18:57:38 +0000

    station

---
+++ models/station_dimension.sql
@@ -0,0 +1,17 @@
+with BIKE as (
+
+select 
+distinct
+start_statio_id,
+start_station_name,
+start_lat,
+start_lng
+
+from {{ source('demo', 'bike') }}
+where RIDE_ID != 'ride_id'
+
+
+
+)
+
+select * FROM BIKE
